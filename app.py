f44856e7b4cf0d3f4cd7b4d94b9b1e5b02ce4d18
sd7456852 <43368117+sd7456852@users.noreply.github.com>
2022-10-23 23:15:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@
 # Line Bot機器人串接與測試
 # """
 #載入LineBot所需要的套件
+import re
 from flask import Flask, request, abort
 
 from linebot import (
@@ -51,8 +52,11 @@ def callback():
 ##### 基本上程式編輯都在這個function #####
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token,message)
+    message = text=event.message.text
+    if re.match('告訴我秘密',message):
+        line_bot_api.reply_message(event.reply_token,TextSendMessage('才不告訴你哩！'))
+    else:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
 
 #主程式
 import os
