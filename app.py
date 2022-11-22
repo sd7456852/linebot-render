2fc87f2562338043333336645c7b4d5eb8c73d6a
sd7456852 <aa7711816@gmail.com>
2022-11-22 09:49:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
-# """
-# Created on Wed Jun  2 21:16:35 2021
-# @author: Ivan
-# 版權屬於「行銷搬進大程式」所有，若有疑問，可聯絡ivanyang0606@gmail.com
+"""
+Created on Wed Jun  2 21:16:35 2021
 
-# Line Bot聊天機器人
-# 第一章 Line Bot申請與串接
-# Line Bot機器人串接與測試
-# """
+@author: Ivan
+版權屬於「行銷搬進大程式」所有，若有疑問，可聯絡ivanyang0606@gmail.com
+
+Line Bot聊天機器人
+第四章 選單功能
+快速回復QuickReply
+"""
 #載入LineBot所需要的套件
-import re
 from flask import Flask, request, abort
 
 from linebot import (
@@ -19,16 +19,15 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import *
-
+import re
 app = Flask(__name__)
 
 # 必須放上自己的Channel Access Token
-line_bot_api = LineBotApi('+WddPIOMZ56D7Tre1fmARy1z1eiZGJ5uJEQut/9vq1O6LsM+6hFBHugc/fwD1+HF/KwMEVqQWz1/4ef/4PMhkPWCA7TZXZVwdqB5dZIelMnkukmZL744cTBSBoW1Ua0aCYI94OidHv+wH4uBVJI36QdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi('你自己的token')
 # 必須放上自己的Channel Secret
-handler = WebhookHandler('e97792d24f1f087c95eec15736713fcd')
+handler = WebhookHandler('你自己的secret')
 
-line_bot_api.push_message('U8d188af1584c5e78ab310184099a1bf5', TextSendMessage(text='你可以開始了'))
-
+line_bot_api.push_message('你自己的ID', TextSendMessage(text='你可以開始了'))
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -54,22 +53,21 @@ def callback():
 def handle_message(event):
     message = text=event.message.text
     if re.match('告訴我秘密',message):
-         flex_message = TextSendMessage(text='以下有雷，請小心',
-                                quick_reply=QuickReply(items=[
-                                    QuickReplyButton(action=MessageAction(label="按我", text="按！")),
-                                    QuickReplyButton(action=MessageAction(label="按我", text="按！")),
-                                    QuickReplyButton(action=MessageAction(label="按我", text="按！")),
-                                    QuickReplyButton(action=MessageAction(label="別按我", text="你按屁喔！爆炸了拉！！")),
-                                    QuickReplyButton(action=MessageAction(label="按我", text="按！")),
-                                    QuickReplyButton(action=MessageAction(label="按我", text="按！")),
-                                    QuickReplyButton(action=MessageAction(label="按我", text="按！")),
-                                    QuickReplyButton(action=MessageAction(label="按我", text="按！")),
-                                    QuickReplyButton(action=MessageAction(label="按我", text="按！"))
-                                ]))
-         line_bot_api.reply_message(event.reply_token, flex_message)
-     else:
-         line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
-    
+        flex_message = TextSendMessage(text='以下有雷，請小心',
+                               quick_reply=QuickReply(items=[
+                                   QuickReplyButton(action=MessageAction(label="按我", text="按！")),
+                                   QuickReplyButton(action=MessageAction(label="按我", text="按！")),
+                                   QuickReplyButton(action=MessageAction(label="按我", text="按！")),
+                                   QuickReplyButton(action=MessageAction(label="別按我", text="你按屁喔！爆炸了拉！！")),
+                                   QuickReplyButton(action=MessageAction(label="按我", text="按！")),
+                                   QuickReplyButton(action=MessageAction(label="按我", text="按！")),
+                                   QuickReplyButton(action=MessageAction(label="按我", text="按！")),
+                                   QuickReplyButton(action=MessageAction(label="按我", text="按！")),
+                                   QuickReplyButton(action=MessageAction(label="按我", text="按！"))
+                               ]))
+        line_bot_api.reply_message(event.reply_token, flex_message)
+    else:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
 #主程式
 import os
 if __name__ == "__main__":
