734b4ffbe9c87833031ebf4630629d8227cc9bdd
sd7456852 <aa7711816@gmail.com>
2023-01-03 15:44:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -263,7 +263,7 @@ def handle_message(event):
         )
         line_bot_api.reply_message(event.reply_token, carousel_template_message)
     elif re.match('台中',message):
-        line_bot_api.reply_message(event.reply_token,TextSendMessage('台中網球中心(收費) \n時間:星期一、星期三 \n晚上7:00~9:00 聯絡人:'))
+        line_bot_api.reply_message(event.reply_token,TextSendMessage('台中網球中心(收費)\n時間:星期一、星期三\n晚上7:00~9:00\n聯絡人:\n地圖:https://goo.gl/maps/dBqGFVxX5XwtUAZx5'))
     elif re.match('請輸入地區 例:台中',message):
         line_bot_api.reply_message(event.reply_token,TextSendMessage(''))
         
