7b5d92f8a077816de22d54bb037d370b04b8499a
sd7456852 <aa7711816@gmail.com>
2023-01-11 10:42:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -132,7 +132,6 @@ import json,requests
 
 app = Flask(__name__)
 # LINE BOT info
-# 必須放上自己的Channel Access Token
 line_bot_api = LineBotApi('+WddPIOMZ56D7Tre1fmARy1z1eiZGJ5uJEQut/9vq1O6LsM+6hFBHugc/fwD1+HF/KwMEVqQWz1/4ef/4PMhkPWCA7TZXZVwdqB5dZIelMnkukmZL744cTBSBoW1Ua0aCYI94OidHv+wH4uBVJI36QdB04t89/1O/w1cDnyilFU=')
 # 必須放上自己的Channel Secret
 handler = WebhookHandler('e97792d24f1f087c95eec15736713fcd')
@@ -155,22 +154,10 @@ def get(city):
     url = 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=' + token + '&format=JSON&locationName=' + str(city)
     Data = requests.get(url)
     Data = (json.loads(Data.text,encoding='utf-8'))['records']['location'][0]['weatherElement']
-    res = json.load(open('card.json','r',encoding='utf-8'))
+    res = [[] , [] , []]
     for j in range(3):
-        bubble = json.load(open('bubble.json','r',encoding='utf-8'))
-        # title
-        bubble['body']['contents'][0]['text'] = city + '未來 36 小時天氣'
-        # time
-        bubble['body']['contents'][1]['contents'][0]['text'] = '{} ~ {}'.format(Data[0]['time'][j]['startTime'][5:-3],Data[0]['time'][j]['endTime'][5:-3])
-        # weather
-        bubble['body']['contents'][3]['contents'][1]['contents'][1]['text'] = Data[0]['time'][j]['parameter']['parameterName']
-        # temp
-        bubble['body']['contents'][3]['contents'][2]['contents'][1]['text'] = '{}°C ~ {}°C'.format(Data[2]['time'][j]['parameter']['parameterName'],Data[4]['time'][j]['parameter']['parameterName'])
-        # rain
-        bubble['body']['contents'][3]['contents'][3]['contents'][1]['text'] = Data[1]['time'][j]['parameter']['parameterName']
-        # comfort
-        bubble['body']['contents'][3]['contents'][4]['contents'][1]['text'] = Data[3]['time'][j]['parameter']['parameterName']
-        res['contents'].append(bubble)
+        for i in Data:
+            res[j].append(i['time'][j])
     return res
 
 # Message event
@@ -179,25 +166,35 @@ def handle_message(event):
     message_type = event.message.type
     user_id = event.source.user_id
     reply_token = event.reply_token
-    if(message_type == 'text'):
-        message = event.message.text
-        if(message[:2] == '天氣'):
-            city = message[3:]
-            city = city.replace('台','臺')
-            if(not (city in cities)):
-                line_bot_api.reply_message(reply_token,TextSendMessage(text="查詢格式為: 天氣 縣市"))
-            else:
-                res = get(city)
-                print(res)
-                line_bot_api.reply_message(reply_token, FlexSendMessage(city + '未來 36 小時天氣預測',res))
-    elif(message_type == 'location'):
-        city = event.message.address[5:8].replace('台','臺')
-        res = get(city)
-        line_bot_api.reply_message(reply_token, FlexSendMessage(city + '未來 36 小時天氣預測',res))
+    message = event.message.text
+    if(message[:2] == '天氣'):
+        city = message[3:]
+        city = city.replace('台','臺')
+        if(not (city in cities)):
+            line_bot_api.reply_message(reply_token,TextSendMessage(text="查詢格式為: 天氣 縣市"))
+        else:
+            res = get(city)
+            line_bot_api.reply_message(reply_token, TemplateSendMessage(
+                alt_text = city + '未來 36 小時天氣預測',
+                template = CarouselTemplate(
+                    columns = [
+                        CarouselColumn(
+                            thumbnail_image_url = 'https://i.imgur.com/Ex3Opfo.png',
+                            title = '{} ~ {}'.format(res[0][0]['startTime'][5:-3],res[0][0]['endTime'][5:-3]),
+                            text = '天氣狀況 {}\n溫度 {} ~ {} °C\n降雨機率 {}'.format(data[0]['parameter']['parameterName'],data[2]['parameter']['parameterName'],data[4]['parameter']['parameterName'],data[1]['parameter']['parameterName']),
+                            actions = [
+                                URIAction(
+                                    label = '詳細內容',
+                                    uri = 'https://www.cwb.gov.tw/V8/C/W/County/index.html'
+                                )
+                            ]
+                        )for data in res
+                    ]
+                )
+            ))
     else:
-        line_bot_api.reply_message(reply_token, TextSendMessage(text=message))    
-
+        line_bot_api.reply_message(reply_token, TextSendMessage(text=message))
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)    
