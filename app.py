204c768f4ab08f9f20db5d60825d27fa329b292e
sd7456852 <aa7711816@gmail.com>
2023-01-18 15:59:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -180,7 +180,7 @@ def handle_message(event):
                     columns = [
                         CarouselColumn(
                             thumbnail_image_url = 'https://i.imgur.com/Ex3Opfo.png',
-                            title = '{} ~ {}'.format(data[0]['startTime'][5:-3],data[0]['endTime'][5:-3]),
+                            title = '{} ~ {}'.format(data[0]['startTime'][5:-3],data[0]['endTime'][10:-3]),
                             text = '天氣狀況 {}\n溫度 {} ~ {} °C\n降雨機率 {}'.format(data[0]['parameter']['parameterName'],data[2]['parameter']['parameterName'],data[4]['parameter']['parameterName'],data[1]['parameter']['parameterName']),
                             actions = [
                                 URIAction(
