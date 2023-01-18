32a4c5d146f466e6bb681703d5771496be3e955d
sd7456852 <aa7711816@gmail.com>
2023-01-18 13:49:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -157,7 +157,7 @@ def get(city):
     res = [[] , [], []]
     for j in range(3):
         for i in Data:
-            res[j].append(i['time'][j])
+            res[j].append(i['time'][j]['startTime'][5:-3])
     return res
 
 # Message event
@@ -181,7 +181,7 @@ def handle_message(event):
                         CarouselColumn(
                             thumbnail_image_url = 'https://i.imgur.com/Ex3Opfo.png',
                                             #.format(Data[0]['time'][j]['startTime'][5:-3],Data[0]['time'][j]['endTime'][5:-3])
-                            title = '{} ~ {}'.format(res[0][2]['startTime'][5:-3],res[0][2]['endTime'][5:-3]),
+                            title = '{} ~ {}'.format(res[0][0]['startTime'][5:-3],res[0][0]['endTime'][5:-3]),
                             text = '天氣狀況 {}\n溫度 {} ~ {} °C\n降雨機率 {}'.format(data[0]['parameter']['parameterName'],data[2]['parameter']['parameterName'],data[4]['parameter']['parameterName'],data[1]['parameter']['parameterName']),
                             actions = [
                                 URIAction(
